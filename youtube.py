5cc6bdc3d6bac0e67a6b9d0698d6486a0c9f5397
WarmMilkCodes <87686876+WarmMilkCodes@users.noreply.github.com>
2023-03-18 17:32:14 -0500

    Update

--- youtube.py
+++ youtube.py
@@ -1,26 +1,23 @@
-import tkinter as tk
-from pytube import YouTube
+import os
+import ffmpeg
 
 def Download_Video():
     url = YouTube(str(link.get()))
-    video = url.streams.first()
-    video.download()
-    tk.Label(window,text = 'Success!', font = 'arial 15 bold', fg='white', bg='black', padx= 2).place(x=485, y=145)
 
-window=tk.Tk()
-window.geometry("600x200")
-window.config(bg="#FF0000")
-window.resizable(width=False,height=False)
-window.title('YouTube Video Downloader')
+    # Ask user for the output directory
+    output_path = filedialog.asksaveasfilename(defaultextension=".mp4")
 
-link = tk.StringVar()
+    # Check if user wants to download audio only
+    if mp3_var.get() == 1:
+        audio_stream = url.streams.filter(only_audio=True).first()
+        audio_file_path = os.path.splitext(output_path)[0] + ".mp3"
+        audio_stream.download(output_path=output_path)
+        audio = ffmpeg.input(output_path)
+        audio = ffmpeg.output(audio, audio_file_path, format='mp3')
+        ffmpeg.run(audio)
+        os.remove(output_path)
+    else:
+        video_stream = url.streams.first()
+        video_stream.download(output_path=output_path)
 
-tk.Label(window,text = '    YouTube Video Downloader    ', font='arial 20 bold', fg="Black", bg="#FF0000").pack()
-tk.Label(window, text = 'Paste your YouTube link below ', font='arial 15 bold', bg='#FF0000').place(x=140,y=60)
-
-link_enter = tk.Entry(window, width = 53,textvariable = link, font = 'arial 15 bold', bg='White').place(x=5,y=100)
-
-tk.Button(window,text = 'DOWNLOAD VIDEO', font = 'arial 15 bold', fg='white', bg='black', padx= 2,command=Download_Video).place(x=190, y=140)
-
-
-window.mainloop()
+    tk.Label(window, text='Success!', font='arial 15 bold', fg='white', bg='black', padx=2).place(x=485, y=145)
